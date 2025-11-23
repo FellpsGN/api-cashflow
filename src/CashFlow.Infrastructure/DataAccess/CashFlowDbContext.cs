a8fa25723b5c2cdbb955ee378068d8ef8657561d
FellpsGN <fellps.program06@gmail.com>
2025-11-22 21:18:07 -0300

    :sparkles: feat(database): Adicionar SQLite via EF Core

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,14 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={Environment.GetEnvironmentVariable("DATABASE_URL")}");
+    }
+}
