692254f6d1f0490a5e1ec0e646389f588b925e20
FellpsGN <fellps.program06@gmail.com>
2025-11-29 13:06:43 -0300

    :bug: fix(database): Criar migration alterando a coluna Date de Expense para timestamptz

---
+++ src/CashFlow.Infrastructure/Migrations/20251129160419_ChangeExpenseDateToTimestamptz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeExpenseDateToTimestamptz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Expenses",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Expenses",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+        }
+    }
+}
