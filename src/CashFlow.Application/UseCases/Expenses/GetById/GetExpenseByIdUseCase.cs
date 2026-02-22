f53e6148c1f3908bea105ccbd483ecca8dac6daa
FellpsGN <fellps.program06@gmail.com>
2026-02-22 16:17:26 -0300

    :sparkles: feat: Criar `GetExpenseByIdUseCase` e adicionar mapeamento de entidade para resposta

---
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.GetById;
+
+public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
+{
+    private readonly IExpenseRepository _expenseRepository;
+    private readonly IMapper _mapper;
+
+    public GetExpenseByIdUseCase(IExpenseRepository repository, IMapper mapper)
+    {
+        _expenseRepository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseExpenseJson> Execute(long id)
+    {
+        var result = await _expenseRepository.GetById(id);
+
+        if (result is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+        
+        return _mapper.Map<ResponseExpenseJson>(result);
+    }
+}
