4ca952a10ff100076d868f0a2aeba4cf163055b1
FellpsGN <fellps.program06@gmail.com>
2026-03-10 12:03:27 -0300

    :warning: feat: Adicionar Delete no contrato de escrita de despesas

--- src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
@@ -5,4 +5,12 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpensesWriteOnlyRepository
 {
     Task Add(Expense expense);
+    
+    /// <summary>
+    /// This function returns TRUE if the deletion was successful otherwise returns FALSE.
+    /// Essa função retorna True se o processo de remover a despesa foi feito com sucesso caso contrário retorna False.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> Delete(long id);
 }
