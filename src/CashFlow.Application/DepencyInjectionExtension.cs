76dbfd6bf026676f4d392dc4d700137b2e0a559c
FellpsGN <fellps.program06@gmail.com>
2026-03-10 12:05:56 -0300

    :sparkles: feat: Criar `DeleteExpenseUseCase` e registrar em DependencyInjection

--- src/CashFlow.Application/DepencyInjectionExtension.cs
+++ src/CashFlow.Application/DepencyInjectionExtension.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Delete;
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
@@ -25,6 +26,6 @@ public static class DepencyInjectionExtension
         services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
         services.AddScoped<IGetAllExpenseUseCase, GetAllExpenseUseCase>();
         services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase>();
-        
+        services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
     }
 }
