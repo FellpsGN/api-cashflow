b257133e17d8b5382f9abee7dea4b536efb981e1
FellpsGN <fellps.program06@gmail.com>
2026-03-15 14:26:11 -0300

    :recycle: refactor: Renomear `RequestRegisterExpenseJson` e `RegisterExpenseValidator` para versões reutilizaveis

--- src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/ExpenseValidator.cs
@@ -2,7 +2,7 @@ using CashFlow.Exception;
 using CashFlow.Communication.Requests;
 using FluentValidation;
 
-namespace CashFlow.Application.UseCases.Expenses.Register;
+namespace CashFlow.Application.UseCases.Expenses;
 
 public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
 {
