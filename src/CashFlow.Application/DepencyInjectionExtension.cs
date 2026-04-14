4bee78d8c24b0ef0079cecc54a8624eb773a5113
FellpsGN <fellps.program06@gmail.com>
2026-04-14 10:38:26 -0300

    :sparkles: feat: Adicionar `GenerateExpensesReportExcelUseCase` no `DependencyInjectionExtension`

--- src/CashFlow.Application/DepencyInjectionExtension.cs
+++ src/CashFlow.Application/DepencyInjectionExtension.cs
@@ -5,6 +5,7 @@ using CashFlow.Application.UseCases.Expenses.Delete;
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Application.UseCases.Expenses.Reports.Excel;
 using CashFlow.Application.UseCases.Expenses.Update;
 
 namespace CashFlow.Application;
@@ -29,5 +30,6 @@ public static class DepencyInjectionExtension
         services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase>();
         services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
         services.AddScoped<IUpdateExpenseUseCase, UpdateExpenseUseCase>();
+        services.AddScoped<IGenerateExpensesReportExcelUseCase, GenerateExpensesReportExcelUseCase>();
     }
 }
